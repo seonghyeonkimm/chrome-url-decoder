346560da76870f5d8fb6397b4fd50d03828b861d
demian <demian@kcd.co.kr>
2021-09-21 17:16:00 +0900

    Update extention scripts

--- background.js
+++ background.js
@@ -1,7 +1,23 @@
-// TODO: (chrome storage) url 체크해서 실행 여부 결정
+// TODO: onLocationChange
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content-script.js']
+  chrome.storage.sync.get('rules', function(result) {
+    const rules = result.rules || [];
+    const urlPatterns = rules.map((item) => {
+      return item && item.urlPattern ? item.urlPattern : undefined;
+    }).filter(Boolean);
+
+    if (urlPatterns.length === 0) {
+      return;
+    }
+
+    const { origin } = new URL(tab.url);
+    if (!urlPatterns.includes(origin)) {
+      return;
+    }
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content-script.js']
+    });
   });
 });
